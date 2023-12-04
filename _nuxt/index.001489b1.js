184ba82325a475d348f151d9d0ae34043c62293b
Allan Jumadiao <allanjumadiao.yns@gmail.com>
2023-12-04 16:01:57 +0800

    deployment

--- _nuxt/index.001489b1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,W as a,A as t,t as s,x as c}from"./entry.0c7f4c6e.js";import{a as o}from"./VCard.e82653b5.js";const r={},n=c("h1",null,"INDEX",-1);function _(d,f){return s(),a(o,{class:"pa-4"},{default:t(()=>[n]),_:1})}const p=e(r,[["render",_]]);export{p as default};
